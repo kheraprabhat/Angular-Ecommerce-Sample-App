8373aa5ee4cfddf96f05327754eca5387b4efeaa
Krishna Kumar <krishna_aim24@yahoo.com>
2015-12-03 18:17:47 +0530

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,35 +1,23 @@
 require.config({
-    baseUrl: 'js',
-    paths: { 
-        angular: './libs/angular', 
-        angularrouter: './libs/angular-route',
-        jquery: './libs/jquery'
+    baseUrl: "js",
+    paths: {
+        angular: "./libs/angular",
+        angularrouter: "./libs/angular-route",
+        jquery: "./libs/jquery"
     },
-    shim: { 
-    	angular: { exports: 'angular' },
-        jquery: { exports: '$' },
-        angularrouter: { deps: ["angular"], exports: 'angularrouter' }
+    shim: {
+        angular: {
+            exports: "angular"
+        },
+        jquery: {
+            exports: "$"
+        },
+        angularrouter: {
+            deps: ["angular"],
+            exports: "angularrouter"
+        }
     }
 });
-
-require([
-    './ralph',
-    './angularrouter',
-    './features/ralphRouter',
-    './features/services/utility',
-
-    './features/directives/breadcrumb',
-
-    './features/controllers/ralphCtrl',
-    './features/controllers/headerCtrl',
-    
-    './features/controllers/homePageCtrl',
-    
-    './features/controllers/categoryCtrl',
-    
-    './features/controllers/breadcrumbCtrl',
-
-    './features/controllers/productCtrl'
-], function(Ralph) {
-    angular.bootstrap(document, ['RALPH']);
+require(["./ralph", "./angularrouter", "./features/ralphRouter", "./features/services/utility", "./features/directives/breadcrumb", "./features/controllers/ralphCtrl", "./features/controllers/headerCtrl", "./features/controllers/homePageCtrl", "./features/controllers/categoryCtrl", "./features/controllers/breadcrumbCtrl", "./features/controllers/productCtrl"], function(Ralph) {
+    angular.bootstrap(document, ["RALPH"]);
 });
