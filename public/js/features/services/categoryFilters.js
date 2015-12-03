a3c489b2602347c244fc9f8834608d7f1bdd9761
Krishna Kumar <krishna_aim24@yahoo.com>
2015-12-03 18:19:08 +0530

    Update categoryFilters.js

--- public/js/features/services/categoryFilters.js
+++ public/js/features/services/categoryFilters.js
@@ -1,34 +1,30 @@
-require(['Ralph'], function(Ralph){
-	Ralph.factory('categoryFilters', ['utility', function(utility){
-		var _pvtVariables = {}, 
-		_pvtMethods = {
-			initFilterForSelectedCategoryProducts: function(scope){
-				scope.brandsForSelectedcategoryProducts = utility.getCategoryFilters({
-					name: 'brandsForSelectedcategoryProducts',
-					key: 'brand',
-					data: utility.getData('allProductsForSelectedcategory')
-				});
-				
-				scope.sectionForSelectedcategoryProducts = utility.getCategoryFilters({
-					name: 'sectionForSelectedcategoryProducts',
-					key: 'productType',
-					data: utility.getData('allProductsForSelectedcategory')
-				});
-
-				scope.typesForSelectedcategoryProducts = utility.getCategoryFilters({
-					name: 'typesForSelectedcategoryProducts',
-					key: 'type',
-					data: utility.getData('allProductsForSelectedcategory')
-				});
-
-				scope.ratingForSelectedcategoryProducts = utility.getCategoryFilters({
-					name: 'ratingForSelectedcategoryProducts',
-					key: 'rating',
-					data: utility.getData('allProductsForSelectedcategory')
-				});
-			}
-		};
-
-		return _pvtMethods;
-	}]);
+require(["Ralph"], function(Ralph) {
+    Ralph.factory("categoryFilters", ["utility", function(utility) {
+        var _pvtVariables = {},
+            _pvtMethods = {
+                initFilterForSelectedCategoryProducts: function(scope) {
+                    scope.brandsForSelectedcategoryProducts = utility.getCategoryFilters({
+                        name: "brandsForSelectedcategoryProducts",
+                        key: "brand",
+                        data: utility.getData("allProductsForSelectedcategory")
+                    });
+                    scope.sectionForSelectedcategoryProducts = utility.getCategoryFilters({
+                        name: "sectionForSelectedcategoryProducts",
+                        key: "productType",
+                        data: utility.getData("allProductsForSelectedcategory")
+                    });
+                    scope.typesForSelectedcategoryProducts = utility.getCategoryFilters({
+                        name: "typesForSelectedcategoryProducts",
+                        key: "type",
+                        data: utility.getData("allProductsForSelectedcategory")
+                    });
+                    scope.ratingForSelectedcategoryProducts = utility.getCategoryFilters({
+                        name: "ratingForSelectedcategoryProducts",
+                        key: "rating",
+                        data: utility.getData("allProductsForSelectedcategory")
+                    });
+                }
+            };
+        return _pvtMethods;
+    }]);
 });
